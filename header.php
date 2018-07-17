5c6a2059a0004cf7e2d856cfedc79c1383f6ef2d
lincuiyang <lincuiyang@163.com>
2018-07-17 23:59:14 +0800

    nothing

--- header.php
+++ header.php
@@ -4,13 +4,38 @@
 		<meta charset="<?php bloginfo('charset'); ?>">
 		<title><?php wp_title(''); ?><?php if(wp_title('', false)) { echo ' :'; } ?> <?php bloginfo('name'); ?></title>
 
+
 		
 		<link href="//www.google-analytics.com" rel="dns-prefetch">
         <link href="<?php echo get_template_directory_uri(); ?>/img/icons/favicon.ico" rel="shortcut icon">
         <link href="<?php echo get_template_directory_uri(); ?>/img/icons/touch.png" rel="apple-touch-icon-precomposed">
 
+          <!-- Set render engine for 360 browser -->
+	  	<meta name="renderer" content="webkit">
+
+		<!-- No Baidu Siteapp-->
+		<meta http-equiv="Cache-Control" content="no-siteapp"/>
+
+		<link rel="icon" type="image/png" href="{{assets}}i/favicon.png">
+
+		<!-- Add to homescreen for Chrome on Android -->
+		<meta name="mobile-web-app-capable" content="yes">
+		<link rel="icon" sizes="192x192" href="{{assets}}i/app-icon72x72@2x.png">
+
+		<!-- Add to homescreen for Safari on iOS -->
+		<meta name="apple-mobile-web-app-capable" content="yes">
+		<meta name="apple-mobile-web-app-status-bar-style" content="black">
+		<meta name="apple-mobile-web-app-title" content="Amaze UI"/>
+		<link rel="apple-touch-icon-precomposed" href="assets/i/app-icon72x72@2x.png">
+
+		<!-- Tile icon for Win8 (144x144 + tile color) -->
+		<meta name="msapplication-TileImage" content="assets/i/app-icon72x72@2x.png">
+		<meta name="msapplication-TileColor" content="#0e90d2">
+
         <!--  -->
         <link href="http://cdn.amazeui.org/amazeui/2.7.2/css/amazeui.min.css" rel="stylesheet" type="text/css">
+        
+		<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
 
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -33,32 +58,22 @@
 		<div class="wrapper">
 
 			<!-- header -->
-			<header class="am-topbar am-topbar-inverse am-topbar-fixed-top">
-			  <div class="am-container">
-			    <h1 class="am-topbar-brand">
-			      <a href="#" class="am-text-ir">Amaze UI</a>
-			    </h1>
+			<header class="header">
+				<div class="container">
+					<h2>
+				      <a href="<?php echo esc_url( home_url( '/' ) ); ?>">C-Ryan's Blog</a>
+				    </h2>
 
-			    <button class="am-topbar-btn am-topbar-toggle am-btn am-btn-sm am-btn-success am-show-sm-only"
-			            data-am-collapse="{target: '#doc-topbar-collapse-4'}"><span class="am-sr-only">导航切换</span> <span
-			        class="am-icon-bars"></span></button>
-
-			    <div class="am-collapse am-topbar-collapse" id="doc-topbar-collapse-4">
-			      <ul class="am-nav am-nav-pills am-topbar-nav">
-			        <li class="am-active"><a href="#">首页</a></li>
-			        <li><a href="#">项目</a></li>
-			        <li class="am-dropdown" data-am-dropdown>
-			          <a class="am-dropdown-toggle" data-am-dropdown-toggle href="javascript:;">
-			            下拉 <span class="am-icon-caret-down"></span>
-			          </a>
-			          <ul class="am-dropdown-content">
-			            <li><a href="#">带我去月球</a></li>
-			            <li><a href="#">还是回地球</a></li>
-			            <li class="am-disabled"><a href="#">臣妾做不到</a></li>
-			          </ul>
-			        </li>
+				    <div class="nav-menu">
+			      		<ul class="">
+			        	<li class=""><a href="#">首页</a></li>
+			       		<li><a href="#">项目</a></li>
+			        
 			      </ul>
 			    </div>
-			  </div>
+				</div>
+
+				
+			  
 			</header>
 			<!-- /header -->
