eb305f223a79dab377e6c257021fde2d1b881ebf
lincuiyang <lincuiyang@163.com>
2018-07-29 18:05:48 +0800

    blog index article stylesheet

--- index.php
+++ index.php
@@ -1,15 +1,80 @@
 <?php get_header(); ?>
 
-	<main role="main" >
-		<div class="container">
-			<!-- section -->
-			<section class="card">
-				<div class="card">
-					
-				</div>
-			</section>
-			
-		</div>
+	<main role="main" class="main">
+        <div class="container">
+            <div class="post-list">
+                <article class="article-meta">
+                    <header>
+                    	<h3 class="am-article-title blog-title">
+        					<a href="#">Google fonts 的字體（display 篇）</a>
+      					</h3>
+                    </header>
+	                <main class="article-content">
+                        <p>
+                        连续加了一个多星期的班，今天总算可以停一停了，打开博客看了下最新的一篇文章已经是半年前写的了，博客对我真的是越发没有存在感了，但是毕竟写了5年的博客，怎么都不忍就此彻底的关掉它。我现在所有的工作和生活都源自于当初对博...                        
+						</p>
+                	</main>
+                	<footer>
+                		<span class="time"><time datetime="2018-03-20T18:08:44+00:00" itemprop="datePublished" pubdate="">4个月前</time>发布</span>
+                        <span class="hr"></span>
+                        <span class="comments">92 条评论</span>
+                        <span class="hr"></span><span class="likes">55 人喜欢</span>    
+                	</footer>
+            	</article>
+            	<hr class="am-article-divider">
+
+
+            	<article class="meta">
+                    <header>
+                    	<h3 class="am-article-title blog-title">
+        					<a href="#">Google fonts 的字體（display 篇）</a>
+      					</h3>
+                    </header>
+	                <main class="article-content">
+                        <p>
+                        连续加了一个多星期的班，今天总算可以停一停了，打开博客看了下最新的一篇文章已经是半年前写的了，博客对我真的是越发没有存在感了，但是毕竟写了5年的博客，怎么都不忍就此彻底的关掉它。我现在所有的工作和生活都源自于当初对博...                        
+						</p>
+                	</main>
+                	<footer>
+                		<span class="time"><time datetime="2018-03-20T18:08:44+00:00" itemprop="datePublished" pubdate="">4个月前</time>发布</span>
+                        <span class="hr"></span>
+                        <span class="comments">92 条评论</span>
+                        <span class="hr"></span><span class="likes">55 人喜欢</span>    
+                	</footer>
+            	</article>
+            	<hr class="am-article-divider">
+
+
+            	<article class="meta">
+                    <header>
+                    	<h3 class="am-article-title blog-title">
+        					<a href="#">Google fonts 的字體（display 篇）</a>
+      					</h3>
+                    </header>
+	                <main class="article-content">
+                        <p>
+                        连续加了一个多星期的班，今天总算可以停一停了，打开博客看了下最新的一篇文章已经是半年前写的了，博客对我真的是越发没有存在感了，但是毕竟写了5年的博客，怎么都不忍就此彻底的关掉它。我现在所有的工作和生活都源自于当初对博...                        
+						</p>
+                	</main>
+                	<footer>
+                		<span class="time"><time datetime="2018-03-20T18:08:44+00:00" itemprop="datePublished" pubdate="">4个月前</time>发布</span>
+                        <span class="hr"></span>
+                        <span class="comments">92 条评论</span>
+                        <span class="hr"></span><span class="likes">55 人喜欢</span>    
+                	</footer>
+            	</article>
+            	<hr class="am-article-divider">
+                
+                                
+                                <!--<nav class="reade_more">-->
+                <!--    <a href="/archives">Reade more...</a>-->
+                <!--</nav>-->
+                <nav class="reade_more">
+                    <span class="page-numbers current">1</span> <a class="page-numbers" href="https://biji.io/page/2" title="第 2 页">2</a> ... <a class="page-numbers" href="https://biji.io/page/11" title="最后页">11</a> <a class="page-numbers" href="https://biji.io/page/2" title="下一页">»</a>                 
+                </nav>
+            </div>
+        </div>
+   
 		
 
 <?php get_sidebar(); ?>
